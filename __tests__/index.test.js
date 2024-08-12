16b9da21dbada0e5eba6e2e55a1845178a3cbe72
Iaroslava <floweryaroslava@mail.ru>
2024-08-12 17:24:07 +0800

    Step 4.3

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,29 +1,29 @@
+import * as fs from 'node:fs';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import diff from '../bin/gendiff.js';
+import path from 'node:path';
+import genDiff from '../src/index.js';
 
-
+// Получает путь к текущему файлу в виде строки
 const __filename = fileURLToPath(import.meta.url);
+// Получает директорию текущего файла
 const __dirname = dirname(__filename);
 
+// Функция для построения пути к файлам в директории
 const getFixturePath = (filename) => path.resolve(__dirname, '..', '__fixtures__', filename);
 
 const FILE1_JSON = getFixturePath('file1.json');
 const FILE2_JSON = getFixturePath('file2.json');
+const RESULT = getFixturePath('result.txt');
 
-// результат сравнения этих 2 файлов:
-const EXPECTED_RESULT = `
-{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}
-`;
+const testData = [
+  [FILE1_JSON, FILE2_JSON, RESULT]
+];
 
-test('diff', () => {
-    const result = diff(FILE1_JSON, FILE2_JSON);
-    expect(result).toEqual(EXPECTED_RESULT.trim());
-  });
+test.each(testData)('gendiff', (file1, file2, expected) => {
+  const result = fs.readFileSync(expected, 'utf8');
+  const myResult = genDiff(file1, file2);
+  console.log('Result:', result);
+  console.log('My Result:', myResult);
+  expect(myResult.trim()).toEqual(result.trim());
+});
