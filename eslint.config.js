98b45aef7afa7f931fb2cfb0d9301c11ef752e49
Iaroslava <floweryaroslava@mail.ru>
2024-08-12 17:39:07 +0800

    Step 4.3 (make test & make lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,23 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import jestPlugin from "eslint-plugin-jest"; // Правильный импорт плагина jest
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.jest, // Добавляем глобальные переменные для Jest
+      },
+    },
+  },
+  pluginJs.configs.recommended, // Используем рекомендованные настройки ESLint
+  {
+    plugins: {
+      jest: jestPlugin, // Подключаем плагин Jest
+    },
+    rules: {
+      ...jestPlugin.configs.recommended.rules, // Добавляем рекомендованные правила для Jest
+    },
+  },
 ];
