8910dcf02f8e391e96cece687eb6f3bd72284eb9
Iaroslava <floweryaroslava@mail.ru>
2024-08-13 16:47:52 +0800

    Step 6

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -14,10 +14,13 @@ const getFixturePath = (filename) => path.resolve(__dirname, '..', '__fixtures__
 
 const FILE1_JSON = getFixturePath('file1.json');
 const FILE2_JSON = getFixturePath('file2.json');
+const FILE1_YML = getFixturePath('file1.yml');
+const FILE2_YML = getFixturePath('file2.yml');
 const RESULT = getFixturePath('result.txt');
 
 const testData = [
   [FILE1_JSON, FILE2_JSON, RESULT],
+  [FILE1_YML, FILE2_YML, RESULT],
 ];
 
 test.each(testData)('gendiff', (file1, file2, expected) => {
