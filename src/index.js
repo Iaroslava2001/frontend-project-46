3197d61ca67c69fcf89ec2ff77c2908707cf43be
Iaroslava <floweryaroslava@mail.ru>
2024-08-13 16:23:07 +0800

    Step 4.5

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import path from 'node:path';
 import _ from 'lodash';
 import parse from './parsers.js';
 
-
 const getFileData = (filePath) => fs.readFileSync(filePath, 'utf8');
 const getFileExtension = (filePath) => path.extname(filePath).split('.')[1];
 
 const genDiff = (filepath1, filepath2) => {
   const data1 = parse(getFileData(filepath1), getFileExtension(filepath1));
   const data2 = parse(getFileData(filepath2), getFileExtension(filepath2));
-    
+
   const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
   const diff = keys.map((key) => {
     if (!_.has(data2, key)) {
@@ -24,9 +23,9 @@ const genDiff = (filepath1, filepath2) => {
         `  - ${key}: ${data1[key]}`,
         `  + ${key}: ${data2[key]}`,
       ].join('\n');
-      }
+    }
     return `    ${key}: ${data1[key]}`;
-    });
-    return (`{\n${diff.join('\n')}\n}`);
+  });
+  return (`{\n${diff.join('\n')}\n}`);
 };
 export default genDiff;
