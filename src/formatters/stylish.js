81555bcbaa9ce027da8e31e58bc3b5c5ccaebb80
Iaroslava <floweryaroslava@mail.ru>
2024-08-18 18:25:45 +0800

    Fix 5

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -12,30 +12,31 @@ const formatNode = (node, depth) => {
 
   const stringify = (key, value, currentDepth, sign = ' ') => `${getIndent(currentDepth, 2)}${sign} ${key}: ${formatNode(value, currentDepth + 1)}`;
 
+  const processNode = (item) => {
+    const { key, value, status } = item;
+    switch (status) {
+      case 'added':
+        return stringify(key, value, depth, '+');
+      case 'deleted':
+        return stringify(key, value, depth, '-');
+      case 'changed': {
+        const { changedValue } = item;
+        return `${stringify(key, value, depth, '-')}\n${stringify(key, changedValue, depth, '+')}`;
+      }
+      case 'withChildren':
+        return stringify(key, value, depth, ' ');
+      case 'unchanged':
+        return stringify(key, value, depth, ' ');
+      default:
+        throw new Error(`Unknown status: '${status}'!`);
+    }
+  };
+
   const currentIndent = getIndent(depth);
   const bracketIndent = getIndent(depth, SPACE_COUNT);
 
   const lines = _.isArray(node)
-    ? node.map((item) => {
-      const { key, value, status } = item;
-
-      switch (status) {
-        case 'added':
-          return stringify(key, value, depth, '+');
-        case 'deleted':
-          return stringify(key, value, depth, '-');
-        case 'changed': {
-          const { changedValue } = item;
-          return `${stringify(key, value, depth, '-')}\n${stringify(key, changedValue, depth, '+')}`;
-        }
-        case 'withChildren':
-          return stringify(key, value, depth, ' ');
-        case 'unchanged':
-          return stringify(key, value, depth, ' ');
-        default:
-          throw new Error(`Unknown status: '${status}'!`);
-      }
-    })
+    ? node.map(processNode)
     : Object.entries(node).map(([key, val]) => `${currentIndent}${key}: ${formatNode(val, depth + 1)}`);
 
   return ['{', ...lines, `${bracketIndent}}`].join('\n');
